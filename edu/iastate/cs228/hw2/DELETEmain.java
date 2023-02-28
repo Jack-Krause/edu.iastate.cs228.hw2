43e1e8ab22a01887a863daf8fe49c63f653b0a54
Jack Krause <jmkrause@iastate.edu>
2023-02-28 17:29:28 -0600

    Initial commit

---
+++ edu/iastate/cs228/hw2/DELETEmain.java
@@ -0,0 +1,9 @@
+package edu.iastate.cs228.hw2;
+
+public class DELETEmain {
+
+    public static void main(String[] args) {
+        Point p = new Point(1,0);
+        System.out.println(p.toString());
+    }
+}
